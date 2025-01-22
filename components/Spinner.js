3fb31233c667d0e2cd61978c089a3139d6651134
theHalid <the_halidcastle@mail.ru>
2025-01-23 03:03:30 +0600

    images added

---
+++ components/Spinner.js
@@ -0,0 +1,5 @@
+import { FadeLoader } from "react-spinners";
+
+export default function Spinner() {
+    return <FadeLoader color={"#1E3A8A"} sppedMultiplier={2} />;
+}
--- models/Product.js
+++ models/Product.js
@@ -1,9 +1,11 @@
+import { images } from "@/next.config";
 import { model, models, Schema } from "mongoose";
 
 const ProductSchema = new Schema({
     title: { type: String, required: true },
     description: { type: String },
     price: { type: Number, required: true },
+    images: [{ type: String }],
 });
 
 export const Product = models.Product || model("Product", ProductSchema);
