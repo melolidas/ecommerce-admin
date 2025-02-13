fe92932af609cae9fdfa9ef86d2b38fd5ffac04b
theHalid <the_halidcastle@mail.ru>
2025-02-13 23:40:22 +0600

    orders done

--- components/Spinner.js
+++ components/Spinner.js
@@ -1,5 +1,12 @@
 import { FadeLoader } from "react-spinners";
 
-export default function Spinner() {
-    return <FadeLoader color={"#1E3A8A"} sppedMultiplier={2} />;
+export default function Spinner({ fullWidth }) {
+    if (fullWidth) {
+        return (
+            <div className="w-full flex justify-center">
+                <FadeLoader color={"#1E3A8A"} speedMultiplier={2} />
+            </div>
+        );
+    }
+    return <FadeLoader color={"#1E3A8A"} speedMultiplier={2} />;
 }
--- pages/categories.js
+++ pages/categories.js
@@ -2,6 +2,7 @@ import Layout from "@/components/Layout";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { withSwal } from "react-sweetalert2";
+import Spinner from "@/components/Spinner";
 
 function Categories({ swal }) {
     const [editedCategory, setEditedCategory] = useState(null);
@@ -9,12 +10,15 @@ function Categories({ swal }) {
     const [parentCategory, setParentCategory] = useState("");
     const [categories, setCategories] = useState([]);
     const [properties, setProperties] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
     useEffect(() => {
         fetchCategories();
     }, []);
     function fetchCategories() {
+        setIsLoading(true);
         axios.get("/api/categories").then((result) => {
             setCategories(result.data);
+            setIsLoading(false);
         });
     }
     async function saveCategory(ev) {
@@ -220,6 +224,15 @@ function Categories({ swal }) {
                         </tr>
                     </thead>
                     <tbody>
+                        {isLoading && (
+                            <tr>
+                                <td colSpan={2}>
+                                    <div className="py-4">
+                                        <Spinner fullWidth={true} />
+                                    </div>
+                                </td>
+                            </tr>
+                        )}
                         {categories.length > 0 &&
                             categories.map((category) => (
                                 <tr key={category._id}>
