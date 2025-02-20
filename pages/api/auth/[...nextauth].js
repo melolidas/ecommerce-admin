e4a2f844c3d5ff58fefcf2529f0b1cf2d225eb60
theHalid <the_halidcastle@mail.ru>
2025-02-21 04:04:17 +0600

    auth made for showcase

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,14 +1,54 @@
-import NextAuth, { getServerSession } from "next-auth";
+// for production
+
+// import NextAuth, { getServerSession } from "next-auth";
+// import GoogleProvider from "next-auth/providers/google";
+// import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
+// import clientPromise from "@/lib/mongodb";
+// import { Admin } from "@/models/Admin";
+// import { mongooseConnect } from "@/lib/mongoose";
+
+// async function isAdminEmail(email) {
+//     mongooseConnect();
+//     return !!(await Admin.findOne({ email }));
+// }
+
+// export const authOptions = {
+//     secret: process.env.SECRET,
+//     providers: [
+//         GoogleProvider({
+//             clientId: process.env.GOOGLE_ID,
+//             clientSecret: process.env.GOOGLE_SECRET,
+//             allowDangerousEmailAccountLinking: true,
+//         }),
+//     ],
+//     adapter: MongoDBAdapter(clientPromise),
+//     callbacks: {
+//         session: async ({ session, token, user }) => {
+//             if (await isAdminEmail(session?.user?.email)) {
+//                 return session;
+//             } else {
+//                 return false;
+//             }
+//         },
+//     },
+// };
+
+// export default NextAuth(authOptions);
+
+// export async function isAdminRequest(req, res) {
+//     const session = await getServerSession(req, res, authOptions);
+//     if (!(await isAdminEmail(session?.user?.email))) {
+//         res.status(401);
+//         res.end();
+//         throw "not an admin";
+//     }
+// }
+
+//for showcase
+import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
 import clientPromise from "@/lib/mongodb";
-import { Admin } from "@/models/Admin";
-import { mongooseConnect } from "@/lib/mongoose";
-
-async function isAdminEmail(email) {
-    mongooseConnect();
-    return !!(await Admin.findOne({ email }));
-}
 
 export const authOptions = {
     secret: process.env.SECRET,
@@ -20,24 +60,6 @@ export const authOptions = {
         }),
     ],
     adapter: MongoDBAdapter(clientPromise),
-    callbacks: {
-        session: async ({ session, token, user }) => {
-            if (await isAdminEmail(session?.user?.email)) {
-                return session;
-            } else {
-                return false;
-            }
-        },
-    },
 };
 
 export default NextAuth(authOptions);
-
-export async function isAdminRequest(req, res) {
-    const session = await getServerSession(req, res, authOptions);
-    if (!(await isAdminEmail(session?.user?.email))) {
-        res.status(401);
-        res.end();
-        throw "not an admin";
-    }
-}
